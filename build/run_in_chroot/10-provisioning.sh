1e8db15279bf19682dab5310dd81999c725c4c53
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2020-04-11 01:20:00 +0200

    Mopidy-Iris and Mopidy-Local added

--- build/run_in_chroot/10-provisioning.sh
+++ build/run_in_chroot/10-provisioning.sh
@@ -8,8 +8,11 @@ wget -q -O - https://apt.mopidy.com/mopidy.gpg | apt-key add -
 wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/buster.list
 apt update
 apt install -y mopidy gstreamer1.0-alsa mopidy-mpd python3-pip
-python3 -m pip install Mopidy-MusicBox-Webclient
 
+################
+# Mopidy-Local # 
+################
+python3 -m pip install Mopidy-Local
 
 ####################
 # Mopidy-AlsaMixer #
@@ -18,13 +21,20 @@ apt install -y python3-dev libasound2-dev python3-pyalsa
 python3 -m pip install Mopidy-AlsaMixer
 apt purge -y python3-dev libasound2-dev
 
+#############################
+# Mopidy-MusicBox-Webclient #
+#############################
+#python3 -m pip install Mopidy-MusicBox-Webclient
+
+###############
+# Mopidy-Iris #
+###############
+python3 -m pip install Mopidy-Iris
+
 
 cat <<EOF >>/etc/mopidy/mopidy.conf 
 [http]
 hostname = 0.0.0.0
-
-[file]
-media_dirs = /media
 
 [audio]
 output = alsasink
@@ -32,6 +42,13 @@ mixer = alsamixer
 
 [alsamixer]
 control = Line Out
+
+[file]
+media_dirs = /media
+
+[local]
+media_dir = /media
+scan_follow_symlinks = true
 EOF
 
 systemctl enable mopidy
