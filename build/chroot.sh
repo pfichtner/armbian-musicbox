84ec0385410ea08ccdee7ef06e2d74c099cb416d
Peter Fichtner <github.07.fichtner@0sg.net>
2020-04-15 19:30:17 +0200

    removed commented lines

--- build/chroot.sh
+++ build/chroot.sh
@@ -17,10 +17,6 @@ prepare_chroot() {
     # Disable preloaded shared library to get everything including networking to work on x86
     backup /etc/ld.so.preload
 
-    echo /etc/resolv.conf && cat /etc/resolv.conf
-    echo ${ROOTFS_DIR}/etc/resolv.conf && cat ${ROOTFS_DIR}/etc/resolv.conf
-
-    #backup /etc/resolv.conf ; cp -ax /etc/resolv.conf ${ROOTFS_DIR}/etc/resolv.conf
     backup ${ROOTFS_DIR}/usr/bin/qemu-arm-static ; cp `which qemu-arm-static` ${ROOTFS_DIR}/usr/bin/
     # Prevent upgraded services from trying to start inside chroot
     backup ${ROOTFS_DIR}/usr/sbin/policy-rc.d
@@ -29,7 +25,6 @@ prepare_chroot() {
 }
 
 finish_chroot() {
-    # rm -f ${ROOTFS_DIR}/etc/resolv.conf ${ROOTFS_DIR}/usr/bin/qemu-arm-static ${ROOTFS_DIR}/usr/sbin/policy-rc.d
     rm -f ${ROOTFS_DIR}/usr/bin/qemu-arm-static ${ROOTFS_DIR}/usr/sbin/policy-rc.d
     [ -r "$BACKUP_DIR/backup.tar" ] && tar -xpf $BACKUP_DIR/backup.tar && rm $BACKUP_DIR/backup.tar
     sync
