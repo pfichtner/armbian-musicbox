09a6b373c2d8b172fae028e3c9ad237b5d6a4dd3
Peter Fichtner <github.07.fichtner@0sg.net>
2020-07-09 22:43:36 +0200

    support for Mopidy-Raspberry-GPIO added

--- build/run_in_chroot/20-install-mopidy.sh
+++ build/run_in_chroot/20-install-mopidy.sh
@@ -75,6 +75,24 @@ EOF
 ###################
 
 
+###############################
+# Mopidy-Raspberry-GPIO START #
+###############################
+# python3 -m pip install Mopidy-Raspberry-GPIO
+
+# cat <<EOF >>/etc/mopidy/mopidy.conf 
+# [raspberry-gpio]
+# enabled = true
+# bcm5 = play_pause,active_low,250
+# bcm6 = volume_down,active_low,250
+# bcm16 = next,active_low,250
+# bcm20 = volume_up,active_low,250
+# EOF
+#############################
+# Mopidy-Raspberry-GPIO END #
+#############################
+
+
 
 echo "mopidy" >/etc/hostname
 # TODO passwords?
